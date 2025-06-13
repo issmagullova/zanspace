7326a2a995ff3c28036ba9b2dd572dd42c9a6911
issmagullova <aminaismagulova20@gmail.com>
2025-06-13 14:16:33 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def webhook():
     return "!", 200
 
 # Flask run
-if name == "__main__":
+if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/{TOKEN}")
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
